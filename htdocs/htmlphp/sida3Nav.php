8bd9912d111a09cf750a8b88b37d3cf33bd171f8
HHasanov <46808740+HHasanov@users.noreply.github.com>
2019-09-24 10:53:54 +0200

    Add files via upload

---
+++ htdocs/htmlphp/sida3Nav.php
@@ -0,0 +1,8 @@
+      <nav>
+        <ul>
+          <li><a href="index.html">Start</a></li>
+          <li><a href="products.php">Produkter</a></li>
+          <li><a href="sida3.php">Varusida 2</a></li>
+          <li><a href="login.html">Logga in</a></li>
+        </ul>
+      </nav>
